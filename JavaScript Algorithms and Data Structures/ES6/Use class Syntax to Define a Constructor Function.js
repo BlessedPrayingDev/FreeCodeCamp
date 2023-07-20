437944bc77287978a49ae348bd0f87e4c9ac3ca9
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-20 17:08:50 +0900

    JavaScript ES6

---
+++ JavaScript Algorithms and Data Structures/ES6/Use class Syntax to Define a Constructor Function.js
@@ -0,0 +1,20 @@
+/*
+* // Only change code below this line
+
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+* */
+
+// My solution
+// Only change code below this line
+class Vegetable {
+    constructor(name) {
+        this.name = name;
+    }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
---
+++ JavaScript Algorithms and Data Structures/ES6/Write Concise Declarative Functions with ES6.js
@@ -0,0 +1,24 @@
+/*
+* // Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear: function(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+* */
+
+// My solution
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
