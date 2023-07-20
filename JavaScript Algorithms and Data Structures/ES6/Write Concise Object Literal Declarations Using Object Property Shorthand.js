52875e96c0ce25b3c36732db22f3a274c7f74e39
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-20 14:26:47 +0900

    JavaScript ES6

---
+++ JavaScript Algorithms and Data Structures/ES6/Write Concise Object Literal Declarations Using Object Property Shorthand.js
@@ -0,0 +1,20 @@
+/*
+* const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  };
+  // Only change code above this line
+};
+* */
+
+// My solution
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return ({
+        name, age, gender
+    });
+    // Only change code above this line
+};
