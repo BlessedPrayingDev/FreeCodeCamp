d1f6a9cb5f4c880ea18af4851960468f2369979c
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-22 07:06:47 +0900

    JavaScript Regular Expressions

--- JavaScript Algorithms and Data Structures/Regular Expressions/Find Characters with Lazy Matching.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Find Characters with Lazy Matching.js
@@ -5,3 +5,14 @@ let result = text.match(myRegex);
 * */
 
 // My solution
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<[a-z0-9]*?>/; // Change this line
+let result = text.match(myRegex);
+let result2 = myRegex.test(text);
+console.log(result)
+console.log(result2)
+
+// Solution
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // it's the answer!
+let result = text.match(myRegex);
