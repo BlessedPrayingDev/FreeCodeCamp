b848f69d7329bd30156ca0e0b2793abf1cbe3958
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-21 09:59:33 +0900

    JavaScript ES6

--- JavaScript Algorithms and Data Structures/ES6/Complete a Promise with resolve and reject.js
+++ JavaScript Algorithms and Data Structures/ES6/Complete a Promise with resolve and reject.js
@@ -12,3 +12,13 @@
  */
 
 // My solution
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if(responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
