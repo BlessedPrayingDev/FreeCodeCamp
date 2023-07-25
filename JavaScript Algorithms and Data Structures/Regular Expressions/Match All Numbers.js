cd18f76c2b591cad2e54d9eec34024e852ce7d1a
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-25 11:03:52 +0900

    JavaScript Regular Expressions

--- JavaScript Algorithms and Data Structures/Regular Expressions/Match All Numbers.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Match All Numbers.js
@@ -5,3 +5,7 @@ let result = movieName.match(numRegex).length;
 * */
 
 // My solution
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
+console.log(result);
