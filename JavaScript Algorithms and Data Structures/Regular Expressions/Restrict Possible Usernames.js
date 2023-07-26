731e553e3f4e6048cce50791a5878770ec098b62
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-26 09:05:06 +0900

    JavaScript Regular Expressions

--- JavaScript Algorithms and Data Structures/Regular Expressions/Restrict Possible Usernames.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Restrict Possible Usernames.js
@@ -5,3 +5,10 @@ let result = userCheck.test(username);
 * */
 
 // My solution
+
+
+// Solution
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+let result = userCheck.test(username);
+console.log(result)
