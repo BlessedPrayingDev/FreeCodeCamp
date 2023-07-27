6514a2b2ac3db4f91358d5d3412457874d71f71c
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-27 09:51:28 +0900

    JavaScript Regular Expressions

--- JavaScript Algorithms and Data Structures/Regular Expressions/Specify Only the Lower Number of Matches.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Specify Only the Lower Number of Matches.js
@@ -4,3 +4,8 @@ let haRegex = /change/; // Change this line
 let result = haRegex.test(haStr);
 * */
 
+// My solution
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/g; // Change this line
+let result = haRegex.test(haStr);
+console.log(result)
