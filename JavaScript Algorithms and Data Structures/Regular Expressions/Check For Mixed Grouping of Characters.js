1e2ee258af6189da6b03d96c627d45bc07b1be91
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-27 17:49:05 +0900

    JavaScript Regular Expressions

---
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Check For Mixed Grouping of Characters.js
@@ -0,0 +1,13 @@
+/*
+* let myString = "Eleanor Roosevelt";
+let myRegex = /False/; // Change this line
+let result = false; // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+* */
+
+// My solution
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+console.log(result);
