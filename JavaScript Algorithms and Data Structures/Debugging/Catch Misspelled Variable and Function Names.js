2907bcf9ea6a586169480cabe07259af9ff66df4
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-28 16:24:25 +0900

    JavaScript Debugging

---
+++ JavaScript Algorithms and Data Structures/Debugging/Catch Misspelled Variable and Function Names.js
@@ -0,0 +1,12 @@
+/*
+*let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+* */
+
+// My solution
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
