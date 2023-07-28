8a9bfc16eba7d23dc52b7893cdc8fff73acb6ef5
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-28 16:28:39 +0900

    JavaScript Debugging

---
+++ JavaScript Algorithms and Data Structures/Debugging/Catch Mixed Usage of Single and Double Quotes.js
@@ -0,0 +1,8 @@
+/*
+*let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+console.log(innerHtml);
+* */
+
+// My solution
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
---
+++ JavaScript Algorithms and Data Structures/Debugging/Catch Unclosed Parentheses, Brackets, Braces and Quotes.js
@@ -0,0 +1,10 @@
+/*
+* let myArray = [1, 2, 3;
+let arraySum = myArray.reduce((previous, current =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+* */
+
+// My solution
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
