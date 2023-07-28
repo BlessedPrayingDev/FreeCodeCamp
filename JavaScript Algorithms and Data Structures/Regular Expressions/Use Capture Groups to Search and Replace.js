81544f7523c9cf2388685dfce212ade480702a7d
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-28 15:36:48 +0900

    JavaScript Regular Expressions

---
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Use Capture Groups to Search and Replace.js
@@ -0,0 +1,12 @@
+/*
+ *let str = "one two three";
+let fixRegex = /change/; // Change this line
+let replaceText = ""; // Change this line
+let result = str.replace(fixRegex, replaceText);
+ */
+
+// My solution
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
