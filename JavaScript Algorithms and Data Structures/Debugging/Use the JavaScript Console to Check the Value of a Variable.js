9c6565d641b07ed0c00d4fcd84cef9b31402c258
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-28 16:08:43 +0900

    JavaScript Debugging

--- JavaScript Algorithms and Data Structures/Debugging/Use the JavaScript Console to Check the Value of a Variable.js
+++ JavaScript Algorithms and Data Structures/Debugging/Use the JavaScript Console to Check the Value of a Variable.js
@@ -8,3 +8,13 @@ a++;
 let sumAB = a + b;
 console.log(sumAB);
 * */
+
+// My solution
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a);
+
+let sumAB = a + b;
+console.log(sumAB);
