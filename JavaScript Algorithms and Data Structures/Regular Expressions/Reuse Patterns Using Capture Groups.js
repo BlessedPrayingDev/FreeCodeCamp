98b17fed955c3ef5f3b0da4d91da06f2654ba159
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-28 15:34:30 +0900

    JavaScript Regular Expressions

--- JavaScript Algorithms and Data Structures/Regular Expressions/Reuse Patterns Using Capture Groups.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Reuse Patterns Using Capture Groups.js
@@ -5,3 +5,13 @@ let result = reRegex.test(repeatNum);
 * */
 
 // My solution
+let repeatNum = "42 42 42";
+let reRegex = /(\d+) \1 \1/g; // Change this line
+let result = reRegex.test(repeatNum);
+console.log(result);
+console.log(repeatNum.match(reRegex));
+
+// Solution
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/;
+let result = reRegex.test(repeatNum);
