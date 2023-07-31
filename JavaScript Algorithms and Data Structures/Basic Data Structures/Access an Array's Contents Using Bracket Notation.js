cfd7cf98dfa531f3c24933dfcc72caa78419108d
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-31 11:38:01 +0900

    JavaScript Basic Data Structures

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Access an Array's Contents Using Bracket Notation.js
@@ -0,0 +1,14 @@
+/*
+*let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+
+// Only change code above this line
+console.log(myArray);
+* */
+
+// My solution
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "e"
+// Only change code above this line
+console.log(myArray);
