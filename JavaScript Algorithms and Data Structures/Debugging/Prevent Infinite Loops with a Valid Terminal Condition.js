a08b58628e18c7d50d9c7617502b4bed68eaf68c
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-31 11:24:36 +0900

    JavaScript Debugging

---
+++ JavaScript Algorithms and Data Structures/Debugging/Prevent Infinite Loops with a Valid Terminal Condition.js
@@ -0,0 +1,16 @@
+/*
+*function myFunc() {
+  for (let i = 1; i != 4; i += 2) {
+    console.log("Still going!");
+  }
+}
+* */
+
+// My solution
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+        console.log("Still going!");
+    }
+}
+
+myFunc();
