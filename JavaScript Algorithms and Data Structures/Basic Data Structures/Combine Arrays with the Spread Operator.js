cd32ccb73d2c20f41a9c084b3740474af3f0a5aa
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-31 13:57:19 +0900

    JavaScript Basic Data Structures

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Combine Arrays with the Spread Operator.js
@@ -0,0 +1,18 @@
+/*
+*function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence; // Change this line
+  return sentence;
+}
+
+console.log(spreadOut());
+* */
+
+// My solution
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+    return sentence;
+}
+
+console.log(spreadOut());
