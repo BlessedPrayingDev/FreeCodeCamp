0295ab77b035c7796dd3eebdac60c1763a8e8ce3
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-31 11:43:54 +0900

    JavaScript Basic Data Structures

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Add Items to an Array with push() and unshift().js
@@ -0,0 +1,21 @@
+/*
+*function mixedNumbers(arr) {
+  // Only change code below this line
+
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+* */
+
+// My solution
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2, 'three')
+    arr.push(7, 'VIII', 9)
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
