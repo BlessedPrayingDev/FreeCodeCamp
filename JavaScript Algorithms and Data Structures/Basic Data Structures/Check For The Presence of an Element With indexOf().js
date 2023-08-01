8a1aec359a44b57c833c9f6d577857b796300919
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-01 16:04:11 +0900

    JavaScript Basic Data Structures

--- JavaScript Algorithms and Data Structures/Basic Data Structures/Check For The Presence of an Element With indexOf().js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Check For The Presence of an Element With indexOf().js
@@ -9,3 +9,14 @@ console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
 * */
 
 // My solution
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    if(arr.indexOf(elem) == -1) {
+        return false
+    } else {
+        return true;
+    }
+    // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
