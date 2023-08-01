0fe76ce91cb44f62e32576e964ef6b80db506aae
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-01 18:38:53 +0900

    JavaScript Basic Data Structures

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Iterate Through All an Array's Items Using For Loops.js
@@ -0,0 +1,26 @@
+/*
+*function filteredArray(arr, elem) {
+  let newArr = [];
+  // Only change code below this line
+
+  // Only change code above this line
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+* */
+
+// My solution
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // Only change code below this line
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i].indexOf(elem) == -1) {
+            newArr.push(arr[i])
+        }
+    }
+    // Only change code above this line
+    return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
