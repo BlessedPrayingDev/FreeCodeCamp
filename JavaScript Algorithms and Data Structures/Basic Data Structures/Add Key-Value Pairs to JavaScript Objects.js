8384c37c0dd0474692b86a20a6065b8cadb955e8
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-03 11:05:25 +0900

    JavaScript Basic Data Structures

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Add Key-Value Pairs to JavaScript Objects.js
@@ -0,0 +1,29 @@
+/*
+*let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// Only change code below this line
+
+// Only change code above this line
+
+console.log(foods);
+* */
+
+// My solution
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+
+// Only change code below this line
+foods.bananas = 13
+foods['grapes'] = 35
+const value = 'strawberries'
+foods[value] = 27
+// Only change code above this line
+
+console.log(foods);
