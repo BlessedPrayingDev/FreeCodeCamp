53af00e8a007f0cb8e1a1cb3dd380284d70c2ecf
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-03 11:20:25 +0900

    JavaScript Basic Data Structures

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Modify an Object Nested Within an Object.js
@@ -0,0 +1,32 @@
+/*
+*let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+// Only change code below this line
+
+// Only change code above this line
+
+console.log(userActivity);
+* */
+
+// My solution
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+        totalUsers: 51,
+        online: 42
+    }
+};
+
+// Only change code below this line
+userActivity.data.online = 45
+// Only change code above this line
+
+console.log(userActivity);
