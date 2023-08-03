e0b4fad30b22f85b2e6dc82865c72c34bd78d5e4
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-03 11:27:09 +0900

    JavaScript Basic Data Structures

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Access Property Names with Bracket Notation.js
@@ -0,0 +1,36 @@
+/*
+*let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
+* */
+
+// My solution
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+    // Only change code below this line
+    return foods[scannedItem];
+    // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
