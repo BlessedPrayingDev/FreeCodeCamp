3b2c3aeaed2fad43e2636e1704a82bfa7c3725f8
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-07 15:21:13 +0900

    JavaScript - Basic Algorithm Scripting

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Finders Keepers.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Finders Keepers.js
@@ -9,9 +9,7 @@ findElement([1, 2, 3, 4], num => num % 2 === 0);
 
 // My solution
 function findElement(arr, func) {
-    let num = 0;
     for(let i = 0; i < arr.length; i++) {
-        console.log(arr[i])
         if(func(arr[i]) === true) {
             return arr[i]
         }
