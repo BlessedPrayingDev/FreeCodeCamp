5dffddf82c3fe3b0ab659d4d71f1b3d123e1a1bb
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-11 17:13:52 +0900

    JavaScript - Basic Algorithm Scripting

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Falsy Bouncer.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Falsy Bouncer.js
@@ -7,3 +7,20 @@ bouncer([7, "ate", "", false, 9]);
 * */
 
 // My solution
+function bouncer(arr) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(Boolean(arr[i])) {
+            newArr.push(arr[i])
+        }
+    }
+    console.log(newArr);
+    return newArr;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+// Other solution
+function bouncer(arr) {
+    return arr.filter(Boolean);
+}
