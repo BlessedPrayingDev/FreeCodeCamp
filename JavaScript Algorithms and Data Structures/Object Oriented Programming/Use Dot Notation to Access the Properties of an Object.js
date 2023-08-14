c38245b343190e0d7385f16932f92f451d1c48f6
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-14 14:49:07 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Use Dot Notation to Access the Properties of an Object.js
@@ -0,0 +1,16 @@
+/*
+*let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Only change code below this line
+* */
+
+// My solution
+let dog = {
+    name: "Spot",
+    numLegs: 4
+};
+// Only change code below this line
+console.log(dog.name);
+console.log(dog.numLegs);
