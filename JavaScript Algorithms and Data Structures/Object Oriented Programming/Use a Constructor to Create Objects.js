c213fb82bb9d56e689c1782db87833e365154388
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-17 13:36:35 +0900

    JavaScript - Object Oriented Programming

--- JavaScript Algorithms and Data Structures/Object Oriented Programming/Use a Constructor to Create Objects.js
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Use a Constructor to Create Objects.js
@@ -8,3 +8,10 @@
 * */
 
 // My solution
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
