33f78cc729943ba25475459d0d66f3c123b03720
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-18 11:32:10 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Extend Constructors to Receive Arguments.js
@@ -0,0 +1,15 @@
+/*
+*
+* function Dog() {
+
+}
+* */
+
+// My solution
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+}
+
+let terrier = new Dog("John", "red")
