fc9084b4355b47d1da00505a554d852249c85f5a
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-18 11:53:38 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Verify an Object's Constructor with instanceof.js
@@ -0,0 +1,17 @@
+/*
+*function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Only change code below this line
+* */
+
+// My solution
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+
+// Only change code below this line
+let myHouse = new House(3);
+
+myHouse instanceof House;
