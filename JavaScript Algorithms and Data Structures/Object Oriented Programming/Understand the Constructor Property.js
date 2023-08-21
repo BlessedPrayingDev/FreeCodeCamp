4b3716a9573717d81dad9e6b75db4a8362695590
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-21 11:55:29 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Understand the Constructor Property.js
@@ -0,0 +1,24 @@
+/*
+*function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+
+}
+* */
+
+// My solution
+function Dog(name) {
+    this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog) {
+        return true;
+    } else {
+        return false;
+    }
+}
