60bc45cdb868988a01c7dbd49e504a131e708249
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-31 13:45:13 +0900

    JavaScript - Object Oriented Programming

--- JavaScript Algorithms and Data Structures/Object Oriented Programming/Understand the Prototype Chain.js
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Understand the Prototype Chain.js
@@ -12,3 +12,13 @@ Dog.prototype.isPrototypeOf(beagle);  // yields true
 * */
 
 // My solution
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
