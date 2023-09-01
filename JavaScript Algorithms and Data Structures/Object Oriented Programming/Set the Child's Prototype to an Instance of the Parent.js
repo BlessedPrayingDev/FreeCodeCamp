0f9354a334d7148face992c8e1d84ff73fffc382
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-01 16:37:17 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Set the Child's Prototype to an Instance of the Parent.js
@@ -0,0 +1,19 @@
+/*
+*function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Only change code below this line
+
+
+let beagle = new Dog();
+* */
+
+// My solution
