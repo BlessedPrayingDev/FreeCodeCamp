5a9a498fa02f8c649904bd668029709c2cf63d47
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-04 12:02:47 +0900

    JavaScript - Object Oriented Programming

--- JavaScript Algorithms and Data Structures/Object Oriented Programming/Set the Child's Prototype to an Instance of the Parent.js
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Set the Child's Prototype to an Instance of the Parent.js
@@ -17,3 +17,19 @@ let beagle = new Dog();
 * */
 
 // My solution
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+    }
+};
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype);
+let beagle = new Dog("John");
+beagle.eat();
