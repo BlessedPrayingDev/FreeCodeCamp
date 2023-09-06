62f21d4848925a0b2abff045c5ed25a1e31bd85c
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-06 15:50:22 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Add Methods After Inheritance.js
@@ -0,0 +1,17 @@
+/*
+*function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+
+
+
+
+// Only change code above this line
+
+let beagle = new Dog();
+* */
+
+// My solution
