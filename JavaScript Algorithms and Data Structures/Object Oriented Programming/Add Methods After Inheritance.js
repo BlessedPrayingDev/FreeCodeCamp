bbcce6fe46cd0cb3455960ad12fed8cb32c1f6a6
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-06 17:04:23 +0900

    JavaScript - Basic Algorithm Scripting

--- JavaScript Algorithms and Data Structures/Object Oriented Programming/Add Methods After Inheritance.js
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Add Methods After Inheritance.js
@@ -15,3 +15,22 @@ let beagle = new Dog();
 * */
 
 // My solution
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function() {
+    console.log("Woof!");
+};
+
+
+
+
+// Only change code above this line
+
+let beagle = new Dog();
