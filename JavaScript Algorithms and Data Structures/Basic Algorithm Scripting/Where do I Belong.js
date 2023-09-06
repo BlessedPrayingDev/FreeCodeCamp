49acb89c02176b8233ae89cdccfc6a4fcc0838c7
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-06 16:05:50 +0900

    JavaScript - Basic Algorithm Scripting

--- JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Where do I Belong.js
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Where do I Belong.js
@@ -7,3 +7,12 @@ getIndexToIns([40, 60], 50);
 * */
 
 // My solution
+function getIndexToIns(arr, num) {
+    arr.sort((a, b) => a - b);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= num) return i;
+    }
+
+    return arr.length;
+}
