c76b1edeced4befd3ef60289a8c1f90ae928abe5
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-06 17:42:41 +0900

    JavaScript - Object Oriented Programming

--- JavaScript Algorithms and Data Structures/Object Oriented Programming/Use a Mixin to Add Common Behavior Between Unrelated Objects.js
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Use a Mixin to Add Common Behavior Between Unrelated Objects.js
@@ -1,3 +1,34 @@
 /*
-*
+*let bird = {
+    name: "Donald",
+    numLegs: 2
+};
+
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+
+// Only change code below this line
 * */
+
+// My solution
+let bird = {
+    name: "Donald",
+    numLegs: 2
+};
+
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+
+// Only change code below this line
+let glideMixin = (obj) => {
+    obj.glide = () => {
+        console.log("I can glide")
+    }
+}
+
+glideMixin(bird);
+glideMixin(boat);
