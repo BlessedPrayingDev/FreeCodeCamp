50c40bd44fbd5f8845b463f0887cf71216892abd
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-06 16:12:47 +0900

    JavaScript - Basic Algorithm Scripting

---
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/Mutations.js
@@ -0,0 +1,17 @@
+/*
+*function mutation(arr) {
+  return arr;
+}
+
+mutation(["hello", "hey"]);
+* */
+
+// My solution
+function mutation(arr) {
+    const test = arr[1].toLowerCase();
+    const target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+        if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+}
