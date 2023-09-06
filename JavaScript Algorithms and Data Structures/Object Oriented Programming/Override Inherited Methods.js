9fe4b9bd6d6a170a82608c154ff5f90dd1502325
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-06 17:07:32 +0900

    JavaScript - Basic Algorithm Scripting

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Override Inherited Methods.js
@@ -0,0 +1,38 @@
+/*
+*function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+* */
+
+// My solution
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird.";
+};
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
