7e8e33433b3741651b6dc18b61df2384e9d1eb90
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-07 17:51:21 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Functional Programming/Learn About Functional Programming.js
@@ -0,0 +1,50 @@
+/*
+*
+*
+* // Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+// const getTea = (numOfCups) => {
+//     const teaCups = [];
+//
+//     for(let cups = 1; cups <= numOfCups; cups += 1) {
+//         const teaCup = prepareTea();
+//         teaCups.push(teaCup);
+//     }
+//     return teaCups;
+// };
+//
+// // Only change code below this line
+// const tea4TeamFCC = null;
+// // // Only change code above this line
+// * */
+
+
+// My solution
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+    const teaCups = [];
+
+    for(let cups = 1; cups <= numOfCups; cups += 1) {
+        const teaCup = prepareTea();
+        teaCups.push(teaCup);
+    }
+    return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+console.log(tea4TeamFCC);
+// Only change code above this line
