fa9746c6e08dcf14bdbbed2d82360cb1330a6dea
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-07 17:45:34 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Use an IIFE to Create a Module.js
@@ -0,0 +1,28 @@
+/*
+*let isCuteMixin = function(obj) {
+  obj.isCute = function() {
+    return true;
+  };
+};
+let singMixin = function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+};
+* */
+
+// My solution
+let funModule = (function() {
+    return {
+        isCuteMixin: function(obj) {
+            obj.isCute = function() {
+                return true;
+            };
+        },
+        singMixin: function(obj) {
+            obj.sing = function() {
+                console.log("Singing to an awesome tune");
+            };
+        }
+    };
+})();
