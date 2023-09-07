e7aea45a11aea3ecdee2992d244d7c8aaf2f32c4
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-07 17:41:50 +0900

    JavaScript - Object Oriented Programming

---
+++ JavaScript Algorithms and Data Structures/Object Oriented Programming/Understand the Immediately Invoked Function Expression (IIFE).js
@@ -0,0 +1,14 @@
+/*
+*function makeNest() {
+  console.log("A cozy nest is ready");
+}
+
+makeNest();
+* */
+
+// My solution
+(function () {
+    console.log("A cozy nest is ready");
+})();
+
+makeNest();
