d9b683a1801af4261f7b88e6fabbc72b25cb7391
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-12 11:27:33 +0900

    JavaScript - Functional Programming

---
+++ JavaScript Algorithms and Data Structures/Functional Programming/Avoid Mutations and Side Effects Using Functional Programming.js
@@ -0,0 +1,13 @@
+// /*
+// *// The global variable
+// let fixedValue = 4;
+//
+// function incrementer() {
+//     // Only change code below this line
+//
+//
+//     // Only change code above this line
+// }
+// * */
+
+// My solution
