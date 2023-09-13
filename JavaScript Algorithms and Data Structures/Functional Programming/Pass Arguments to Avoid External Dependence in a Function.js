a61219941242ed38642e73b740b54a86344675d6
BlessedPrayingDevGru <gasaret@gmail.com>
2023-09-13 14:02:52 +0900

    JavaScript - Functional Programming

---
+++ JavaScript Algorithms and Data Structures/Functional Programming/Pass Arguments to Avoid External Dependence in a Function.js
@@ -0,0 +1,13 @@
+// /*
+// *// The global variable
+// let fixedValue = 4;
+//
+// // Only change code below this line
+// function incrementer() {
+//
+//
+//     // Only change code above this line
+// }
+// * */
+
+// My solution
